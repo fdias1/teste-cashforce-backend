b1bfe29c66369fb9ad7e642c8116b2c522d85481
Fábio Dias <fabbiodiaz@gmail.com>
2021-01-12 10:13:18 -0300

    adicionada rota para providers

--- api/src/routes/v1.js
+++ api/src/routes/v1.js
@@ -3,7 +3,9 @@ const routes = Router()
 
 // Controllers disponíveis
 const orders = require('../controllers/ordersController')
+const providers = require('../controllers/providersController')
 
 routes.use('/orders',orders)
+routes.use('/providers',providers)
 
 module.exports = routes
